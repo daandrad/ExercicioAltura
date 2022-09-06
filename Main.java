2490a8f0a4b0ab51b8197135f078f23f6959d313
Daniel Andrade <46009616+daandrad@users.noreply.github.com>
2022-09-06 18:37:02 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,28 @@
+
+ 
+import java.util.Scanner;
+ 
+public class Main {
+ 
+ public static void main(String[] args){
+ 
+ Scanner input = new Scanner(System.in);
+ 
+ System.out.println("Informe o valor da base: ");
+ double base = input.nextDouble();
+ 
+ System.out.println("Informe o valor da altura: ");
+ double altura = input.nextDouble();
+ 
+ double areaRetangulo = retangulo(base, altura);
+ 
+ System.out.println(areaRetangulo);
+ 
+ }
+ 
+ public static double retangulo(double base, double altura){
+ 
+ return base * altura;
+ 
+ }
+}
